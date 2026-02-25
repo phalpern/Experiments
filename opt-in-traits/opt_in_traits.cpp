d4740b3db9b5b6074bf0ce713e4cdca2295e9353
Pablo Halpern <phalpern@halpernwightsoftware.com>
2026-02-25 17:50:44 -0500

    checkpoint for opt-in traits

--- opt-in-traits/opt_in_traits.cpp
+++ opt-in-traits/opt_in_traits.cpp
@@ -4,92 +4,131 @@ namespace xstd {
 
 using namespace std;
 
-enum class trait_scope { disabled, noninherited, inherited };
-
-template <template <class> class Trait>
-struct opt_in_defaults
-{
-  constexpr static trait_scope default_scope = trait_scope::noninherited;
-  constexpr static trait_scope max_scope     = trait_scope::inherited;
+enum class trait_heritability {
+  default_heritability, not_heritable, heritable
 };
 
 template <template <class> class Trait,
-          trait_scope Scope = opt_in_defaults<Trait>::default_scope>
+          trait_heritability heritability =
+              trait_heritability::default_heritability,
+          bool Condition = true>
 struct opt_in { };
 
-template <template <class> class Trait>
-using opt_out = opt_in<Trait, trait_scope::disabled>;
+template <template <class> class Trait,
+          trait_heritability Heritability =
+              trait_heritability::default_heritability,
+          bool Condition = true>
+using opt_out = opt_in<Trait, Heritability, !Condition>;
 
-template <class Tp, template <class> class Trait, trait_scope>
-struct __opt_bundle
+template <class Sp, trait_heritability Hy, bool Cond>
+struct __opt_in_tuple
 {
-  constexpr static trait_scope Scope = trait_scope::disabled;
-  using Targ  = void;
+  // Bundle of opt_in properties for a specific type.
+  using scope                                       = Sp;
+  constexpr static trait_heritability heritability  = Hy;
+  constexpr static bool               condition     = Cond;
 };
 
-template <class Tp, template <class> class Trait, trait_scope Sp>
-requires requires(Tp& t) { t.operator opt_in<Trait, Sp>(); }
-struct __opt_bundle<Tp, Trait, Sp>
+template <class Tp, template <class> class Trait,
+          trait_heritability Hy, bool Cond,
+          trait_heritability dflt_Hy, bool dflt_Cond>
+struct __get_opt_in_tuple
 {
-  template <class B> static B getTarg(opt_in<Trait, Sp> (B::*)());
+  // Primary template chosen when neither 'opt_in' nor 'opt_out' operator is
+  // defined for class 'Tp'.
+  using type = __opt_in_tuple<void, dflt_Hy, dflt_Cond>;
+};
 
-  constexpr static trait_scope Scope = Sp;
-  using Targ  = decltype(getTarg(&Tp::operator opt_in<Trait, Scope>));
+template <class Tp, template <class> class Trait,
+          trait_heritability Hy, bool Cond,
+          trait_heritability dflt_Hy, bool dflt_Cond>
+requires requires(Tp& t) { t.operator opt_in<Trait, Hy, Cond>(); }
+struct __get_opt_in_tuple
+{
+  // Specialization chosen when either 'opt_in' nor 'opt_out' operator is
+  // defined for class 'Tp' or one if it's base classes.
+  template <class B> static B getScope(opt_in<Trait, Hy, Cond> (B::*)());
+
+  using scope = decltype(getScope(&Tp::operator opt_in<Trait, heritability,
+                                                        Cond>));
+  constexpr static trait_heritability heritability =
+              (Hy == trait_heritability::default_heritability ? dflt_hy : Hy);
+
+  using type = __opt_in_tuple<scope, heritibility, Cond>;
 };
 
 template <class B1, class B2,
-          class B1Targ = typename B1::Targ, class B2Targ = typename B2::Targ>
-struct __deeper_bundle {
-  // Return deeper bundle
-  static_assert(is_same_v<B1, B2> || ! is_same_v<B1Targ, B2Targ>,
-                "Cannot opt in/out of same trait twice");
-  using type = conditional_t<is_base_of_v<B1Targ, B2Targ>, B2, B1>;
+          class B1Scope = typename B1::scope,
+          class B2Scope = typename B2::scope>
+struct __deeper_tuple
+{
+  // Return opt-in tuple having the more-derived scope
+
+  // If B1 and B2 are different, then they must have differen scopes
+  static_assert(is_same_v<B1, B2> || ! is_same_v<B1Scope, B2Scope>,
+                "Cannot opt in/out of same trait twice in same scope");
+  using type = conditional_t<is_base_of_v<B1Scope, B2Scope>, B2, B1>;
 };
 
 template <class B1, class B2>
-struct __deeper_bundle<B1, B2, void, void>
+struct __deeper_tuple<B1, B2, void, void>
 {
-  // Neither B1::Targ nor B2::Targ is a base class of the other.  Choose
-  // arbitrary one.
+  // Both B1 and B2 are defaulted; Choose an arbitrary one.
   using type = B1;
 };
 
-template <class B1, class B2, class B1Targ>
-struct __deeper_bundle<B1, B2, B1Targ, void>
+template <class B1, class B2, class B1Scope>
+struct __deeper_tuple<B1, B2, B1Scope, void>
 {
-  // B2Targ is void, choose B1.
+  // B2 is defaulted, choose B1.
   using type = B1;
 };
 
-template <class B1, class B2, class B2Targ>
-struct __deeper_bundle<B1, B2, void, B2Targ>
+template <class B1, class B2, class B2Scope>
+struct __deeper_tuple<B1, B2, void, B2Scope>
 {
-  // B1Targ is void, choose B2.
+  // B1 is defaulted, choose B2.
   using type = B2;
 };
 
 template <class B1, class B2>
-using __deeper_bundle_t = typename __deeper_bundle<B1, B2>::type;
+using __deeper_tuple_t = typename __deeper_tuple<B1, B2>::type;
 
-template <class Tp, template <class> class Trait>
+template <class Tp, template <class> class Trait, bool AllowInheritance,
+          trait_heritability dflt_Hy, bool dflt_Cond>
 struct __detect_opt_in_imp
 {
-  using d_bundle = __opt_bundle<Tp, Trait, trait_scope::disabled>;
-  using n_bundle = __opt_bundle<Tp, Trait, trait_scope::noninherited>;
-  using i_bundle = __opt_bundle<Tp, Trait, trait_scope::inherited>;
+  using th = trait_heritability;
 
-  // find the bundle representing the operator instance at the  most-derived
+  using dt_tuple = typename _get_opt_in_tuple<Tp, trait,
+                                              th::default_heritability,
+                                              true, dflt Hy, dflt_Cond>;
+  using nt_tuple = typename _get_opt_in_tuple<Tp, trait, th::not_heritable
+                                              true, dflt Hy, dflt_Cond>;
+  using it_tuple = typename _get_opt_in_tuple<Tp, trait, th::not_heritable
+                                              true, dflt Hy, dflt_Cond>;
+  using df_tuple = typename _get_opt_in_tuple<Tp, trait,
+                                              th::default_heritability,
+                                              false, dflt Hy, dflt_Cond>;
+  using nf_tuple = typename _get_opt_in_tuple<Tp, trait, th::not_heritable
+                                              false, dflt Hy, dflt_Cond>;
+  using if_tuple = typename _get_opt_in_tuple<Tp, trait, th::not_heritable
+                                              false, dflt Hy, dflt_Cond>;
+
+  // find the tuple representing the operator instance at the  most-derived
   // level.
-  using deepest = __deeper_bundle_t<__deeper_bundle_t<d_bundle, n_bundle>,
-                                    i_bundle>;
-  constexpr static trait_scope scope = deepest::Scope;
+  using deepest = __deeper_tuple_t<
+    __deeper_tuple_t<__deeper_tuple_t<dt_tuple, nt_tuple>,
+                     __deeper_tuple_t<it_tuple, df_tuple>>,
+    __deeper_tuple_t<nf_tuple, if_tuple>>;
 
-  static_assert(scope <= opt_in_defaults<Trait>::max_scope,
-                "Cannot declare this trait as inherited");
+  constexpr static trait_heritability heritability = deepest::Heritability;
 
-  constexpr static bool value = (scope == trait_scope::disabled  ? false :
-                                 scope == trait_scope::inherited ? true :
-                                 is_same_v<Tp, typename deepest::Targ>);
+  ==>
+  static_assert(heritability <= opt_in_defaults<Trait>::max_heritability,
+                "Cannot declare this trait as heritable");
+
+  constexpr static bool value = deepest::condition;
 };
 
 template <class Tp, template <class> class Trait>
@@ -108,8 +147,8 @@ namespace xstd {
 template <>
 struct opt_in_defaults<proj::mytrait>
 {
-  constexpr static trait_scope default_scope = trait_scope::noninherited;
-  constexpr static trait_scope max_scope     = trait_scope::inherited;
+  constexpr static trait_heritability default_heritability = trait_heritability::not_heritable;
+  constexpr static trait_heritability max_heritability     = trait_heritability::heritable;
 };
 
 }
@@ -134,7 +173,7 @@ static_assert(! proj::mytrait<Y1>::value);
 static_assert(! proj::mytrait<Z1>::value);
 
 struct X2 {
-  operator xstd::opt_in<proj::mytrait, xstd::trait_scope::inherited>();
+  operator xstd::opt_in<proj::mytrait, xstd::trait_heritability::heritable>();
 };
 
 struct Y2 : X2 { };
@@ -146,7 +185,7 @@ static_assert(  proj::mytrait<Y2>::value);
 static_assert(  proj::mytrait<Z2>::value);
 
 struct X3 {
-  operator xstd::opt_in<proj::mytrait, xstd::trait_scope::inherited>();
+  operator xstd::opt_in<proj::mytrait, xstd::trait_heritability::heritable>();
 };
 
 struct Y3 : X3 {
