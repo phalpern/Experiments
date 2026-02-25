c1a3f09ad772dc11043d2f9dc7a26f794e8af854
Pablo Halpern <phalpern@halpernwightsoftware.com>
2026-02-25 17:53:41 -0500

    Support CRTP in nested traits

--- opt-in-traits/nested_trait.t.cpp
+++ opt-in-traits/nested_trait.t.cpp
@@ -1,10 +1,12 @@
 // nested_trait.t.cpp                                                 -*-C++-*-
 
-#include <cassert>
 #include <iostream>
 
 #if __cplusplus > 201101
-# include <type_traits>
+# define test_assert(c) static_assert(c, "")
+#else
+# include <cassert>
+# define test_assert(c) assert(c)
 #endif
 
 namespace bsl {
@@ -32,26 +34,71 @@ struct MatchAnyType
     MatchAnyType(const T&);  // IMPLICIT
 };
 
+template <template <class> class Trait, class T = void>
+struct TraitWrapper;
+
 template <template <class> class Trait>
-struct TraitWrapper
+struct TraitWrapper<Trait, void>
 {
 };
 
-#if USE_OPERATOR_METHOD
+template <template <class> class Trait>
+struct TraitWrapperBase : TraitWrapper<Trait, void>
+{
+};
+
+template <template <class> class Trait, class T>
+struct TraitWrapper : TraitWrapperBase<Trait>
+{
+};
+
 template <class T, template <class> class Trait, TraitWrapper<Trait> (T::*)()>
-class OpWrap
+class OpMatch
+{
+};
+
+template <std::size_t SZ, template <class> class Trait, class T>
+struct DetectNestedTrait_Imp2 : bsl::false_type
+{
+};
+
+template <template <class> class Trait, class T>
+struct DetectNestedTrait_Imp2<sizeof(char), Trait, T>
+{
+    template <class U>
+    static char check(OpMatch<U, Trait, &U::operator TraitWrapper<Trait> > *x);
+    template <class U>
+    static int  check(MatchAnyType);
+
+    enum { value = sizeof(check<T>(0)) == 1 };
+};
+
+template <template <class> class Trait, class T>
+struct DetectNestedTrait_Imp2<sizeof(short), Trait, T> : bsl::true_type
 {
 };
 
 template <template <class> class Trait, class T>
 struct DetectNestedTrait_Imp
 {
-    template <class U>
-    static char check(OpWrap<U, Trait, &U::operator TraitWrapper<Trait> > *x);
-    template <class U>
-    static int  check(MatchAnyType);
+    static T& tref();
 
-    enum { value = sizeof(check<T>(0)) == 1 };
+    // template <class U>
+    // static char check(OpMatch<U, Trait, &U::operator TraitWrapper<Trait> > *x);
+    template <class U>
+    static char check(TraitWrapper<Trait> x, int);
+    template <class U>
+    static short check(TraitWrapper<Trait, U> x, int);
+    template <class U>
+    static int check(TraitWrapperBase<Trait> x, int);
+    template <class U>
+    static long long check(MatchAnyType, ...);
+
+    static_assert(sizeof(check<T>(tref(), 0)) != sizeof(int),
+                  "Nested-trait `TYPE` argument must match class name");
+
+    enum { value = DetectNestedTrait_Imp2<sizeof(check<T>(tref(), 0)),
+                                          Trait, T>::value };
 };
 
 template <template <class> class Trait, class T>
@@ -63,34 +110,8 @@ struct DetectNestedTrait :
 # define BSLMF_DECLARE_NESTED_TRAIT(TRAIT) \
     operator bslmf::TraitWrapper<TRAIT>()
 
-#else // if ! USE_OPERATOR_METHOD
-template <class T, template <class> class Trait,
-          bsl::true_type (T::*)(TraitWrapper<Trait>)>
-class FnWrap
-{
-};
-
-template <template <class> class Trait, class T>
-struct DetectNestedTrait_Imp
-{
-    template <class U>
-    static char check(FnWrap<U, Trait, &U::declareNestedTrait> *);
-    template <class U>
-    static int  check(MatchAnyType);
-
-    enum { value = sizeof(check<T>(0)) == 1 };
-};
-
-template <template <class> class Trait, class T>
-struct DetectNestedTrait :
-    bsl::integral_constant<bool, DetectNestedTrait_Imp<Trait, T>::value>
-{
-};
-
-# define BSLMF_DECLARE_NESTED_TRAIT(TRAIT) \
-    bsl::true_type declareNestedTrait(bslmf::TraitWrapper<TRAIT>)
-
-#endif // USE_OPERATOR_METHOD
+# define BSLMF_DECLARE_NESTED_TRAIT2(TRAIT, T)    \
+    operator bslmf::TraitWrapper<TRAIT, T>()
 
 }  // close package namespace
 
@@ -120,23 +141,45 @@ struct Z : X
 {
 };
 
+template <class T>
+struct CRTP_Base
+{
+    BSLMF_DECLARE_NESTED_TRAIT2(MyTrait, T);
+};
+
+struct D : CRTP_Base<D>
+{
+};
+
+struct E
+{
+    // Simulate a cut-and-paste error
+    BSLMF_DECLARE_NESTED_TRAIT2(MyTrait, D);
+};
+
 int main()
 {
-    assert((  bslmf::DetectNestedTrait<MyTrait, W>::value));
-    assert((  bslmf::DetectNestedTrait<MyTrait, X>::value));
-    assert((! bslmf::DetectNestedTrait<MyTrait, int>::value));
-    assert((! bslmf::DetectNestedTrait<MyTrait, Y>::value));
-    assert((! bslmf::DetectNestedTrait<MyTrait, Z>::value));
+    test_assert((  bslmf::DetectNestedTrait<MyTrait, W>::value));
+    test_assert((  bslmf::DetectNestedTrait<MyTrait, X>::value));
+    test_assert((! bslmf::DetectNestedTrait<MyTrait, int>::value));
+    test_assert((! bslmf::DetectNestedTrait<MyTrait, Y>::value));
+    test_assert((! bslmf::DetectNestedTrait<MyTrait, Z>::value));
+    test_assert((  bslmf::DetectNestedTrait<MyTrait, D>::value));
+    test_assert((! bslmf::DetectNestedTrait<MyTrait, E>::value));
 
-    assert((  MyTrait<W>::value));
-    assert((  MyTrait<X>::value));
-    assert((! MyTrait<int>::value));
-    assert((! MyTrait<Y>::value));
-    assert((! MyTrait<Z>::value));
+    test_assert((  MyTrait<W>::value));
+    test_assert((  MyTrait<X>::value));
+    test_assert((! MyTrait<int>::value));
+    test_assert((! MyTrait<Y>::value));
+    test_assert((! MyTrait<Z>::value));
+    test_assert((  MyTrait<D>::value));
+    test_assert((! MyTrait<E>::value));
 
-    assert((  OtherTrait<W>::value));
-    assert((! OtherTrait<X>::value));
-    assert((! OtherTrait<int>::value));
-    assert((  OtherTrait<Y>::value));
-    assert((! OtherTrait<Z>::value));
+    test_assert((  OtherTrait<W>::value));
+    test_assert((! OtherTrait<X>::value));
+    test_assert((! OtherTrait<int>::value));
+    test_assert((  OtherTrait<Y>::value));
+    test_assert((! OtherTrait<Z>::value));
+    test_assert((! OtherTrait<D>::value));
+    test_assert((! OtherTrait<E>::value));
 }
